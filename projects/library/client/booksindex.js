9f6e46e4a09549ae6f109cb7a18a8dbd0f5e2dd5
m3ia <meianatividad@gmail.com>
2022-08-30 16:00:52 -0700

    Create basic view for all books

--- projects/library/client/booksindex.js
+++ projects/library/client/booksindex.js
@@ -1,18 +1,27 @@
 async function showBooks(){
-    const URL = 'http://localhost:8080/api/books';
-    const response = await fetch(URL);
-    const responseBooks = await response.json();
-    console.log(responseBooks); 
-    for (let book of responseBooks){
-        const card = `<div class="col-4">
+  const URL = 'http://localhost:8080/api/books';
+  const response = await fetch(URL);
+  const responseBooks = await response.json();
+  console.log(responseBooks); 
+  for (let book of responseBooks){
+    const card = 
+    `
+      <div class="col-4">
         <div class="card">
-        <div class="card-body"
-        <h5 class="class-title">${book.title}
+          <div class="card-header">
+            <h5 class="class-title">${book.title}</h5>
+          </div>
+          <div class="card-body"
+          </div>  
+          <div class="card-footer">
+            <button type="button" class="btn btn-default edit-btn">Edit</button>
+            <button type="button" class="btn btn-danger delete-btn">Delete</button>
+          </div>
         </div>
-        </div>
-        </div>`
-        document.getElementById("books").innerHTML = document.getElementById("books").innerHTML + card;
-    }
+      </div>
+    `
+    document.getElementById("books").innerHTML = document.getElementById("books").innerHTML + card;
+  }
 }
 
 showBooks();
